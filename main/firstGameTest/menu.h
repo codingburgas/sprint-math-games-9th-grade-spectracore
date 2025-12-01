934efd1c8950caa926a8e5eeba4dd0f3ddb45d50
iglapchev24 <IGLapchev24@codingburgas.bg>
2025-12-01 23:31:25 +0200

    Add header file menu.h and a menu.cppfile

---
+++ main/firstGameTest/menu.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<string>
+using namespace std;
+
+void clearScreen();
+
+void showMainMenu(const string& title, const string& playerName);
+void showGuideMenu();
+void showLanguageMenu();
+void showLanguageMenuPart2();
+void showDifficultyMenu();
+void showDifficultyMenuPart2();
+void showCreditsMenu();
+
